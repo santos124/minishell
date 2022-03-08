40f35224a5b9be313d5a8549fede3e1c36a88c7e
Waltyr Adina <wadina@at-r5.msk.21-school.ru>
2022-03-09 01:32:49 +0300

    rename

--- parser/replace_env.c
+++ parser/replace_env.c
@@ -1,33 +1,33 @@
 #include "../minishell.h"
 
-static char	*replace_str(char *line, char *old, char *new, int start)
+static char	*replace_str(char *str, char *old, char *new, int start)
 {
+	char	*ret;
 	int		i;
 	int		j;
 	int		k;
-	char	*result;
-	int		res_len;
+	int		len;
 
 	i = 0;
 	j = 0;
 	k = 0;
-	res_len = ft_strlen(line) - ft_strlen(old) + ft_strlen(new);
-	result = malloc(sizeof(*line) * res_len);
-	while (i < res_len && i < start)
+	len = ft_strlen(str) - ft_strlen(old) + ft_strlen(new);
+	ret = (char *)malloc(sizeof(*str) * len);
+	while (i < start && i < len)
 	{
-		result[i] = line[i];
+		ret[i] = str[i];
 		i++;
 	}
 	k = i + ft_strlen(old) + 1;
 	while (new[j])
-		result[i++] = new[j++];
-	while (line[k])
-		result[i++] = line[k++];
-	result[i] = '\0';
-	return (result);
+		ret[i++] = new[j++];
+	while (str[k])
+		ret[i++] = str[k++];
+	ret[i] = '\0';
+	return (ret);
 }
 
-char	*replace_env(char *line, int *i, t_env *envp)
+char	*replace_env(char *str, int *i, t_env *envp)
 {
 	int		begin;
 	char	*env_key;
@@ -37,9 +37,9 @@ char	*replace_env(char *line, int *i, t_env *envp)
 	begin = *i;
 	env_val = ft_strdup(""); //, all);
 	(*i)++;
-	while (line[*i] && (line[*i] == '_' || ft_isalnum(line[*i])))
+	while (str[*i] && (str[*i] == '_' || ft_isalnum(str[*i])))
 		(*i)++;
-	env_key = ft_substr(line, begin + 1, *i - begin - 1);
+	env_key = ft_substr(str, begin + 1, *i - begin - 1);
 	temp = envp;
 	while (temp)
 	{
@@ -50,17 +50,17 @@ char	*replace_env(char *line, int *i, t_env *envp)
 		}
 		temp = temp->next;
 	}
-	line = replace_str(line, env_key, env_val, begin);
+	str = replace_str(str, env_key, env_val, begin);
 	*i = begin + ft_strlen(env_val) - 1;
 	free(env_key);
-	return (line);
+	return (str);
 }
 
-char	*get_last_exit_code(char *line, t_all *all, int *i)
+char	*get_last_exit_code(char *str, t_all *all, int *i)
 {
 	char	*exit_code;
 
 	exit_code = ft_itoa(all->errnum);
-	line = replace_str(line, "?", exit_code, *i);
-	return (line);
+	str = replace_str(str, "?", exit_code, *i);
+	return (str);
 }
