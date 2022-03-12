3c148d462a493e7e3a9108f4a7f8637cc6659801
Waltyr Adina <wadina@at-r7.msk.21-school.ru>
2022-03-12 19:21:41 +0300

    shlvl.c

--- shlvl.c
+++ shlvl.c
@@ -1,24 +1,23 @@
 #include "minishell.h"
 
-static void	ft_sub_shlvl(t_env *tmp, int level) //+
+static void	ft_sub_shlvl(t_env *tmp, int level)
 {
 	if (!tmp->val[0])
 	{
-		free_null((void**)&tmp->sep);
+		free_null((void **)&tmp->sep);
 		tmp->sep = ft_strdup("=");
-		free_null((void**)&tmp->val);
+		free_null((void **)&tmp->val);
 		tmp->val = ft_strdup("1");
 	}
 	else
 	{
 		level = ft_atoi(tmp->val) + 1;
-		free_null((void**)&tmp->val);
+		free_null((void **)&tmp->val);
 		tmp->val = ft_itoa(level);
 	}
 }
 
-
-void	shlvl_check(t_all *all) //+
+void	shlvl_check(t_all *all)
 {
 	t_env	*tmp;
 	int		level;
@@ -29,7 +28,7 @@ void	shlvl_check(t_all *all) //+
 	{
 		if (!ft_strcmp(tmp->key, "SHLVL"))
 		{
-			ft_sub_shlvl( tmp, level);
+			ft_sub_shlvl(tmp, level);
 			return ;
 		}
 		tmp = tmp->next;
